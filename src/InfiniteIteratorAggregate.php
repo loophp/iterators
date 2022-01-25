4c0a7979a982d1c30a5678216fd900fe4fd66c7b
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-25 20:02:33 +0100

    feat: Add `InfiniteIteratorAggregate`.

---
+++ src/InfiniteIteratorAggregate.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace loophp\iterators;
+
+use Generator;
+use Iterator;
+use IteratorAggregate;
+use Traversable;
+
+/**
+ * @template TKey
+ * @template T
+ *
+ * @implements IteratorAggregate<TKey, T>
+ */
+final class InfiniteIteratorAggregate implements IteratorAggregate
+{
+    /**
+     * @var CachingIteratorAggregate<TKey, T>
+     */
+    private CachingIteratorAggregate $iterator;
+
+    /**
+     * @param Iterator<TKey, T> $iterator
+     */
+    public function __construct(Iterator $iterator)
+    {
+        $this->iterator = new CachingIteratorAggregate($iterator);
+    }
+
+    /**
+     * @return Generator<TKey, T>
+     */
+    public function getIterator(): Traversable
+    {
+        yield from $this->iterator;
+
+        yield from $this;
+    }
+}
---
+++ tests/unit/InfiniteIteratorAggregateTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace tests\loophp\iterators;
+
+use LimitIterator;
+use loophp\iterators\InfiniteIteratorAggregate;
+use loophp\iterators\NormalizeIteratorAggregate;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \loophp\iterators
+ */
+final class InfiniteIteratorAggregateTest extends TestCase
+{
+    public function testBasic(): void
+    {
+        $input = static function () {
+            yield from range('a', 'c');
+        };
+
+        $iterator = new NormalizeIteratorAggregate(
+            new LimitIterator(
+                (new InfiniteIteratorAggregate($input()))->getIterator(),
+                0,
+                5
+            )
+        );
+
+        $expected = ['a', 'b', 'c', 'a', 'b'];
+        self::assertSame($expected, iterator_to_array($iterator));
+    }
+}
