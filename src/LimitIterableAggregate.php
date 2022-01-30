0a1955e6b52440c38fdd6c50677de6889f835fca
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-01-30 21:53:38 +0100

    feat: Add `LimitIterableAggregator`.

---
+++ src/LimitIterableAggregate.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace loophp\iterators;
+
+use Generator;
+use IteratorAggregate;
+use LimitIterator;
+use Traversable;
+
+/**
+ * @template TKey
+ * @template T
+ *
+ * @implements IteratorAggregate<TKey, T>
+ */
+final class LimitIterableAggregate implements IteratorAggregate
+{
+    /**
+     * @var iterable<TKey, T>
+     */
+    private iterable $iterable;
+
+    private int $limit;
+
+    private int $offset;
+
+    /**
+     * @param iterable<TKey, T> $iterable
+     */
+    public function __construct(iterable $iterable, int $offset = 0, int $limit = -1)
+    {
+        $this->iterable = $iterable;
+        $this->offset = $offset;
+        $this->limit = $limit;
+    }
+
+    /**
+     * @return Generator<TKey, T>
+     */
+    public function getIterator(): Traversable
+    {
+        yield from new LimitIterator(
+            (new IterableIteratorAggregate($this->iterable))->getIterator(),
+            $this->offset,
+            $this->limit
+        );
+    }
+}
---
+++ tests/unit/LimitIterableAggregateTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace tests\loophp\iterators;
+
+use Generator;
+use loophp\iterators\LimitIterableAggregate;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @internal
+ * @coversDefaultClass \loophp\iterators
+ */
+final class LimitIterableAggregateTest extends TestCase
+{
+    public function testBasic(): void
+    {
+        $input = static function (): Generator {
+            yield from range('a', 'c');
+        };
+
+        $iterator = new LimitIterableAggregate($input());
+
+        $expected = ['a', 'b', 'c'];
+        self::assertSame($expected, iterator_to_array($iterator));
+
+        $iterator = new LimitIterableAggregate($input(), 1);
+
+        $expected = [1 => 'b', 2 => 'c'];
+        self::assertSame($expected, iterator_to_array($iterator));
+
+        $iterator = new LimitIterableAggregate($input(), 1, 1);
+
+        $expected = [1 => 'b'];
+        self::assertSame($expected, iterator_to_array($iterator));
+    }
+}
