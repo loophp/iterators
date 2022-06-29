d58062e524795c7e81fe5d4528782df8563e68a6
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-06-29 11:00:55 +0200

    Fix typing information.

--- src/SimpleCachingIteratorAggregate.php
+++ src/SimpleCachingIteratorAggregate.php
@@ -25,7 +25,9 @@ use IteratorAggregate;
 final class SimpleCachingIteratorAggregate implements IteratorAggregate
 {
     /**
-     * @var CachingIterator<TKey, T>
+     * // TODO: Remove this when PSalm 5 is released.
+     * @psalm-var CachingIterator<TKey, T>
+     * @phpstan-var CachingIterator<TKey, T, Iterator<TKey, T>>
      */
     private CachingIterator $iterator;
 
