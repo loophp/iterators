72cd437a010cb875de9288b7ac619c0a361803b9
Yevhen <themastir@gmail.com>
2024-07-26 13:27:20 +0300

    add RecursiveIteratorAggregateIterator

---
+++ src/RecursiveIteratorAggregateIterator.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace loophp\iterators;
+
+use Generator;
+use Traversable;
+use IteratorAggregate;
+
+class RecursiveIteratorAggregateIterator implements IteratorAggregate
+{
+    private array $stack = [];
+
+    public function __construct(Traversable $input)
+    {
+        $this->stack[] = $input instanceof \IteratorAggregate ? $input->getIterator() : $input;
+    }
+
+    public function getDepth()
+    {
+        return count($this->stack)-1;
+    }
+
+    public function getIterator() : Generator
+    {
+        while (true) {
+            while (!empty($this->stack) && !end($this->stack)->valid()) {
+                array_pop($this->stack);
+            }
+            if (empty($this->stack)) return;
+            $current = end($this->stack)->current();
+            yield $current;
+            end($this->stack)->next();
+            $this->stack[] = $current instanceof \IteratorAggregate ? $current->getIterator() : $current;
+        }
+    }
+
+}
